f85a3fa129077429e0050703f13eec500c39164e
Paravi Khullar <66310260+paravikhullar@users.noreply.github.com>
2020-07-11 16:30:34 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,25 @@
+class Chain{
+    constructor(bodyA, point2){
+        var options = {
+            bodyA: bodyA,
+            pointB: point2,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+
+    display(){
+
+        if (this.sling.bodyA){
+
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.sling.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# AngryBirds3.5-C-28
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
